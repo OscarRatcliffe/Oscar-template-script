36a296adc0988ff250d070ffa72b5da29581a8a7
Oscar <cyberskely@gmail.com>
2023-10-30 18:47:28 +0000

    Added in template folders

---
+++ rust/target/debug/deps/rust-15d640cdf33266df.d
@@ -0,0 +1,5 @@
+D:\Programming\Template\create\rust\target\debug\deps\rust-15d640cdf33266df.exe: src\main.rs
+
+D:\Programming\Template\create\rust\target\debug\deps\rust-15d640cdf33266df.d: src\main.rs
+
+src\main.rs:
---
+++ rust/target/debug/rust.d
@@ -0,0 +1,1 @@
+D:\Programming\Template\create\rust\target\debug\rust.exe: D:\Programming\Template\create\rust\src\main.rs
